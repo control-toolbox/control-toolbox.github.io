4be5d6380ed99da3451d8dbe35242e97e4de95de
Olivier Cots <66357348+ocots@users.noreply.github.com>
2023-08-18 16:37:23 +0200

    foo

---
+++ assets/js/documentation.js
@@ -0,0 +1,15 @@
+/* from control-toolbox.org/docs/pkg-name/stable/
+
+/* google analytics */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-J27VDFHJW2');
+
+/* ct favicon */
+var favicon = document.createElement('link');
+favicon.type = 'image/x-icon';
+favicon.rel = 'icon';
+favicon.href = 'https://control-toolbox.org/assets/img/ct-logo.svg';
+document.head.appendChild(favicon);
+
