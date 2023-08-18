5083fd4aa805f51654bffd5d9263e28a2e2d5bed
Olivier Cots <66357348+ocots@users.noreply.github.com>
2023-08-18 18:04:50 +0200

    foo

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -25,4 +25,11 @@ window.onload = function() {
     footer.innerHTML = "<p>© 2023 control-toolbox</p>"
     document.body.appendChild(footer);
 
+    var f = document.createElement("FOOTER");
+    document.body.appendChild(f);
+    var p = document.createElement("P");
+    var txt = document.createTextNode("Copyright ©, 2019");
+    p.appendChild(txt);
+    f.appendChild(p);
+
 };
